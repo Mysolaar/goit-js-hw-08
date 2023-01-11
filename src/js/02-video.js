1b5b11fde9c509c3eb653b546c1de58eba47cdb5
Magdalena Grądek <magdalena.gradek@gmail.com>
2023-01-11 14:19:55 +0100

    zad 1 + 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Vimeo from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const playerElem = document.getElementById("vimeo-player");
+const player = new Vimeo(playerElem);
+
+player.on(
+    "timeupdate",
+    throttle(() => {
+        player.getCurrentTime()
+            .then(currentTime => {
+                localStorage.setItem("videoplayer-current-time", currentTime);
+            });
+    }, 1000)
+);
+
+const savedTime = localStorage.getItem("videoplayer-current-time");
+savedTime === null
+    ? player.setCurrentTime(0)
+    : player.setCurrentTime(savedTime);
